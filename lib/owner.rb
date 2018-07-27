77f9d8042e99b53f837510bc80278cb401918d00
april-guerin <aprilguerin@yahoo.com>
2018-07-27 20:13:41 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,32 @@
 class Owner
-  # code goes here
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+  end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  
+  def species()
+    @species = "human"
+  end
+  def species
+    @species
+  end
+  def say_species
+    "I am a human."
+  end
+  
+  def pets
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+  
+  def buy_fish(name)
+    
+  end
+end
 
- end
